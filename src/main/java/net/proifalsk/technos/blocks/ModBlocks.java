886a101ade536bf18fd844edc8b0da780354d1f3
proifalsk <aldoshin.r.a@gmail.com>
2025-01-26 23:23:59 +0300

    Added item class and screwdriver.png; P.S. textures doesnt work!
    Signed-off-by: proifalsk <aldoshin.r.a@gmail.com>

---
+++ src/main/java/net/proifalsk/technos/blocks/ModBlocks.java
@@ -0,0 +1,9 @@
+package net.proifalsk.technos.blocks;
+
+public class ModBlocks {
+
+
+    public static void registerModBlocks() {
+
+    }
+}
---
+++ src/main/java/net/proifalsk/technos/items/ModItems.java
@@ -0,0 +1,37 @@
+package net.proifalsk.technos.items;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import net.proifalsk.technos.Technos;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static Item SCREWDRIVER;
+
+    public static void registerModItems() {
+        SCREWDRIVER = register("screwdriver", Item::new, new Item.Settings());
+
+        setModItemsIntoGroups();
+
+        Technos.LOGGER.info("Technos Item Registry: SUCCESS");
+    }
+
+    private static void setModItemsIntoGroups() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
+            //Items in TOOLS group
+            entries.add(SCREWDRIVER);
+        });
+    }
+
+    private static Item register(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Technos.MOD_ID, path));
+        return Items.register(registryKey, factory, settings);
+    }
+}
