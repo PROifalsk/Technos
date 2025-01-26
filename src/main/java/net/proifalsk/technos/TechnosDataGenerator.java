cdb2a31b2595c78e941646365f05502833cfbf97
proifalsk <aldoshin.r.a@gmail.com>
2025-01-26 18:26:04 +0300

    First commit

---
+++ src/main/java/net/proifalsk/technos/TechnosDataGenerator.java
@@ -0,0 +1,11 @@
+package net.proifalsk.technos;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TechnosDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
