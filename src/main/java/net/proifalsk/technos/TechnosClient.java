dd941028807b0a7999c1b9df4670a6d5d90d0626
proifalsk <aldoshin.r.a@gmail.com>
2025-01-26 18:49:05 +0300

    Added client and sources
    Signed-off-by: proifalsk <aldoshin.r.a@gmail.com>

---
+++ src/main/java/net/proifalsk/technos/TechnosClient.java
@@ -0,0 +1,10 @@
+package net.proifalsk.technos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TechnosClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
